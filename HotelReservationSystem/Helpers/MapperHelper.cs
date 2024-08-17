ea50a67ce5f9934da08008404518ae5df0205045
mohamedabdelaty-98 <abdelatym775@gmail>
2024-08-17 03:37:38 +0300

    Complete Tasks in this card  "Set up the solution structure" on board

---
+++ HotelReservationSystem/Helpers/MapperHelper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+
+namespace HotelReservationSystem.Helpers
+{
+    public static class MapperHelper
+    {
+        public static IMapper mapper { get; set; }
+
+        public static IEnumerable<TResult> Map<TResult>(this IQueryable source)
+        {
+            return source.ProjectTo<TResult>(mapper.ConfigurationProvider);
+        }
+        public static TResult MapeOne<TResult>(this object source)
+        {
+            return mapper.Map<TResult>(source);
+        }    
+    }
+}
