8377c72f65ddcb9c3f658146aaa672867766db3a
omar-el-sayed <omarsayed281@gmail.com>
2024-08-17 16:12:32 +0300

    Merge Raghad branch

---
+++ HotelReservationSystem/Models/RoomImage.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.Models
+{
+    public class RoomImage : BaseEntity
+    {
+        public string ImageURL { get; set; }
+        [MaxLength(2000)]
+        public string Description { get; set; }
+        [ForeignKey("room")]
+        public int? RoomId { get; set; }
+        public  Room? room { get; set; }
+
+    }
+}
