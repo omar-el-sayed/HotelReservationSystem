7e9ab770d44bd8928dfdfcd460ce41c3239d22c7
mohamedabdelaty-98 <abdelatym775@gmail>
2024-08-17 04:11:08 +0300

    Some Update in repo and adding  2 functions

--- HotelReservationSystem/Repositories/GenericRepository.cs
+++ HotelReservationSystem/Repositories/GenericRepository.cs
@@ -12,15 +12,19 @@ namespace HotelReservationSystem.Repositories
 
         public IQueryable<T> Get(Expression<Func<T, bool>> predicate)
             => GetAll().Where(predicate);
+        public IQueryable<TResult> Get<TResult>(Expression<Func<T, bool>> predicate,
+         Expression<Func<T, TResult>> selector)
+            =>_context.Set<T>().Where(predicate).Select(selector);
 
         public T? GetById(int id)
             => GetAll().FirstOrDefault(e => e.Id == id);
 
         public T? GetByIdWithTracking(int id)
-            => _context.Set<T>().Where(e => !e.IsDeleted && e.Id == id)
-                    .AsTracking()
-                    .FirstOrDefault();
-
+            =>GetAll().AsTracking().FirstOrDefault(x => x.Id == id);
+            //=> _context.Set<T>().Where(e => !e.IsDeleted && e.Id == id)
+            //        .AsTracking()
+            //        .FirstOrDefault();
+        public void HardDelete(int id)=>_context.Set<T>().Where(x=>x.Id==id).ExecuteDelete();
         public T Add(T entity)
         {
             _context.Set<T>().Add(entity);
