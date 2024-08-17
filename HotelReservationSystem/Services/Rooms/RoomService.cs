1ab0548e402abed659c3428fd997c57f6da78993
omar-el-sayed <omarsayed281@gmail.com>
2024-08-18 01:51:40 +0300

    conflicts

--- HotelReservationSystem/Services/Rooms/RoomService.cs
+++ HotelReservationSystem/Services/Rooms/RoomService.cs
@@ -6,19 +6,14 @@ using HotelReservationSystem.Services.PictureRooms;
 
 namespace HotelReservationSystem.Services.Rooms
 {
-    public class RoomService(IGenericRepository<Room> repo) : IRoomService
+    public class RoomService(IGenericRepository<Room> repo, IPictureRoomService pictureRoomService) : IRoomService
     {
         private readonly IGenericRepository<Room> repo;
         private readonly IPictureRoomService pictureRoomService;
 
-        public RoomService(IGenericRepository<Room> Repo,IPictureRoomService pictureRoomService)
-        {
-            repo = Repo;
-            this.pictureRoomService = pictureRoomService;
-            }
         public RoomDTO Add(RoomDTO roomDTO)
         {
-            var room =roomDTO.MapeOne<Room>();
+            var room = roomDTO.MapeOne<Room>();
             repo.Add(room);
             repo.SaveChanges();
             return roomDTO;
@@ -50,7 +45,7 @@ namespace HotelReservationSystem.Services.Rooms
         public IEnumerable<RoomDTO> GetAvailableRoom()
         {
             var rooms = repo.Get(r => r.AvailableStatus == AvailableStatus.available);
-            var rooms = repo.GetAll();
+            //var rooms = repo.GetAll();
             return rooms.Map<RoomDTO>();
         }
 
