4b8286360519fb70b4faa937c29461138b5d5758
omar-el-sayed <omarsayed281@gmail.com>
2024-08-19 19:27:53 +0300

    dtos

--- HotelReservationSystem/Profiles/RoomProfile.cs
+++ HotelReservationSystem/Profiles/RoomProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using HotelReservationSystem.DTOs.Room;
 using HotelReservationSystem.Models;
+using HotelReservationSystem.ViewModels.Room;
 
 namespace HotelReservationSystem.Profiles
 {
@@ -9,6 +10,8 @@ namespace HotelReservationSystem.Profiles
         public RoomProfile()
         {
             CreateMap<Room, RoomDTO>().ReverseMap();
+            CreateMap<CreateRoomViewModel, RoomDTO>().ReverseMap();
+            CreateMap<UpdateRoomViewModel, UpdateRoomDto>().ReverseMap();
         }
     }
 }
--- HotelReservationSystem/ViewModels/Room/CreateRoomViewModel.cs
+++ HotelReservationSystem/ViewModels/Room/CreateRoomViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace HotelReservationSystem.ViewModels.Room
+﻿using HotelReservationSystem.DTOs;
+
+namespace HotelReservationSystem.ViewModels.Room
 {
     public class CreateRoomViewModel
     {
+        public int Id { get; set; }
+        public RoomType RoomType { get; set; }
+        public decimal Price { get; set; }
+        public int NumOfBed { get; set; }
+        public int MaxOccupancy { get; set; }
+        public double RoomSize { get; set; }
+        public AvailableStatus AvailableStatus { get; set; }
+        public int RoomNumber { get; set; }
+        public List<PictureRoomDTO>? RoomPictures { get; set; }
+        public List<RoomFacilityDTO>? RoomFacilities { get; set; }
     }
 }
