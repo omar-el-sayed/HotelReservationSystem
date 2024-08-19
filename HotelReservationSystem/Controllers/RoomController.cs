a228e2df72fd4ed8d5823fb3b63e9bd78bcb5893
omar-el-sayed <omarsayed281@gmail.com>
2024-08-19 19:25:17 +0300

    Controllers

--- HotelReservationSystem/Controllers/RoomController.cs
+++ HotelReservationSystem/Controllers/RoomController.cs
@@ -1,4 +1,7 @@
-﻿using HotelReservationSystem.Mediators.Room;
+﻿using HotelReservationSystem.DTOs.Room;
+using HotelReservationSystem.Helpers;
+using HotelReservationSystem.Mediators.Room;
+using HotelReservationSystem.Services.Rooms;
 using HotelReservationSystem.ViewModels.Room;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,30 +9,38 @@ namespace HotelReservationSystem.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class RoomController(IRoomMediator roomMediator) : BaseController
+    public class RoomController(IRoomMediator roomMediator, IRoomService roomService) : BaseController
     {
         [HttpGet("getall")]
         public IActionResult GetAvailableRooms()
         {
-            return Ok();
+            var availableRooms = roomService.GetAvailableRoom();
+            return Ok(availableRooms);
         }
 
         [HttpPost("add")]
         public IActionResult AddRoom(CreateRoomViewModel roomVM)
         {
+            roomService.Add(roomVM.MapeOne<RoomDTO>());
             return Ok();
         }
 
         [HttpPut("edit")]
         public IActionResult UpdateRoom(UpdateRoomViewModel roomVM)
         {
-            return Ok();
+            if (roomService.Update(roomVM.MapeOne<UpdateRoomDto>()))
+                return Ok();
+            else
+                return BadRequest();
         }
 
         [HttpDelete("delete/{id}")]
         public IActionResult DeleteRoom(int id)
         {
-            return Ok();
+            if (roomService.Delete(id))
+                return Ok();
+            else
+                return BadRequest();
         }
     }
 }
