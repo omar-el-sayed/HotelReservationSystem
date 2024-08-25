0febdd3a774b7d538d1b96d89e0960883a439ae7
Shrouk Hassan <68817581+ShrouqHassan@users.noreply.github.com>
2024-08-25 23:15:07 +0300

    phase two Models

---
+++ HotelReservationSystem/Models/Invoice.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelReservationSystem.Models
+{
+    public class Invoice:BaseEntity
+    {
+        public DateTime InvoiceDate { get; set; }   
+        public decimal InvoiceAmount { get; set; }
+        public int ReservationID { get; set; } 
+        public List<Reservation> Reservations { get; set; }
+    }
+}
---
+++ HotelReservationSystem/Models/Reservation.cs
@@ -0,0 +1,25 @@
+﻿using Stripe;
+using System.Collections.ObjectModel;
+
+namespace HotelReservationSystem.Models
+{
+    public class Reservation:BaseEntity
+    {
+        public Reservation()
+        {
+        }
+        public DateTime CheckinDate { get; set; }
+        public DateTime CheckoutDate { get; set; }
+        public decimal TotalPrice {  get; set; }
+        public ReservationStatus Status { get; set; }
+        public int NumOfGuests { get; set; }
+        public List<Room> Rooms { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public int PaymentId { get; set; }
+        public Payment Payment { get; set; }
+        public int InvoiceId { get; set; }
+        public Invoice Invoice { get; set; }
+
+    }
+}
